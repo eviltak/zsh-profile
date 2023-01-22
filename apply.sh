565228f0cb3d54047cbf8051c40df2ba971f6743
Arav Singhal <singhal.arav@gmail.com>
2023-01-22 16:56:44 -0600

    Fix leftwm config path in apply.sh

--- apply.sh
+++ apply.sh
@@ -5,4 +5,4 @@ SRCDIR=$(dirname -- "$( readlink -f -- "$0"; )");
 curl -L git.io/antigen > $HOME/antigen.zsh
 ln -sf "$SRCDIR/.zshrc" "$SRCDIR/.p10k.zsh" "$SRCDIR/.antigenrc" "$HOME"
 mv $HOME/.config/leftwm/config.toml $HOME/.config/leftwm/config.toml.bak
-cp leftwm-config.toml $HOME/.config/leftwm/config.toml
+cp "$SRCDIR/leftwm-config.toml" $HOME/.config/leftwm/config.toml
